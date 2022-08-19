d3ad65750f79bb5d83bf7a4caa58b9533b50bb19
Lucas Pereira <lpereira349@gmail.com>
2022-08-19 16:09:12 -0300

    Prime

---
+++ prime.js
@@ -0,0 +1,46 @@
+function snapCracklePrime(maxValue){
+    let array = []
+
+    for(let i = 1; i <= maxValue; i++){
+
+            if(primos(i)){
+                array.push("Prime")
+            }
+
+            else if(i % 5 == 0 && i % 2 == 1 && primos(i)){
+                array.push("SnapCracklePrime")
+            }else if(i % 5 == 0 && i % 2 == 1 ){
+                array.push("SnapCrackle")
+            }else if(i % 2 == 1 && primos(i)){
+                array.push("SnapPrime")
+            }else if(i % 2 == 1){
+                array.push("Snap")
+            } else if(i % 5 == 0){
+                array.push("Crackle")
+            }else{
+                array.push(i)}
+        }
+
+
+    return array
+}
+console.log(snapCracklePrime(15))
+
+
+
+function primos(extra){
+
+    for(let i = 2; i < extra; i++ ){
+        if(extra % i == 0){
+            return false  
+        }  
+    } 
+    if(extra > 1){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+//console.log(primos(1))
